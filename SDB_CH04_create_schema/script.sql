915564ca16d303cc71e6b8dcc4d1413f5878b811
silvesterkkk <silvester.kim@gmail.com>
2020-01-01 23:13:48 +1100

    update schema create script

--- SDB_CH04_create_schema/script.sql
+++ SDB_CH04_create_schema/script.sql
@@ -1,5 +1,12 @@
+-- apply below in pgAdmin
+
 CREATE SCHEMA phil
     AUTHORIZATION postgres;
 
 COMMENT ON SCHEMA phil
     IS 'PHL project data schema';
+
+
+-- OR below in psql cmd
+
+CREATE SCHEMA phil;
